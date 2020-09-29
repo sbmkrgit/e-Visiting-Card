15d312e4b88d9a816d135f6a55afa20a76c3908a
Shubham <babukumar9931@gmail.com>
2020-09-30 02:18:07 +0530

    Added Form In GenerateQR Page

--- lib/generate.dart
+++ lib/generate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class Generate extends StatefulWidget {
   @override
@@ -9,12 +10,114 @@ class _GenerateState extends State<Generate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Generate Card"),
-      ),
-      body: Center(
-        child: Text('Generate Card'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Generate Card"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: ListView(
+            children: <Widget>[
+              Form(
+                //autovalidate: true,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.zero,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.perm_identity)),
+                        validator:
+                            RequiredValidator(errorText: "Can't be Empty !"),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              labelText: "Email",
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.email)),
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "Can't be Empty !"),
+                            EmailValidator(errorText: 'Not Valid !'),
+                          ])),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: "Company Name",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.work)),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Designation",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.description)),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        decoration: InputDecoration(
+                            labelText: "Web-Site",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.web_asset)),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        decoration: InputDecoration(
+                            labelText: "LinkedIn URL",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.web)),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              labelText: "Contact Number",
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.phone)),
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "Can't be Empty !"),
+                          ])),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: RaisedButton.icon(
+                        elevation: 5.0,
+                        color: Colors.green,
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'SUBMIT',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
