9d0853e98f652db3e7edf9d0bbd1bbea70d8fe58
Shubham <babukumar9931@gmail.com>
2020-10-02 03:29:55 +0530

    Added Save details functionality in scanner

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 import 'dart:async';
 
 class Scanner extends StatefulWidget {
@@ -11,6 +17,39 @@ class Scanner extends StatefulWidget {
 
 class _ScannerState extends State<Scanner> {
   String result = "Hey there !";
+
+  ScreenshotController screenshotController = ScreenshotController();
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
+
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+
+    final info = statuses[Permission.storage].toString();
+    print(info);
+    _toastInfo(info);
+  }
+
+  _toastInfo(String info) {
+    Fluttertoast.showToast(msg: info, toastLength: Toast.LENGTH_SHORT);
+  }
+
+  Future<void> _captureAndSavePng() async {
+    screenshotController.capture().then((File image) async {
+      _toastInfo("Capture Done");
+      setState(() {});
+      ImageGallerySaver.saveImage(image.readAsBytesSync());
+      _toastInfo("File Saved to Gallery");
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
 
   Future _scanQR() async {
     try {
@@ -44,11 +83,41 @@ class _ScannerState extends State<Scanner> {
     return Scaffold(
       appBar: AppBar(
         title: Text("QR Scanner"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.help,
+                color: Colors.white,
+              ),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                //to be implemented
+              }),
+          IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                _captureAndSavePng();
+              })
+        ],
       ),
-      body: Center(
-        child: Text(
-          result,
-          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+      body: Screenshot(
+        controller: screenshotController,
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: Text(
+                  result,
+                  style: new TextStyle(
+                      color: Colors.orange,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
